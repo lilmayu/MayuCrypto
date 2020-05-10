dd46f434bc9ae94dd24ae466bd985bfae40ffa69
Sergey Kuroedov <skuroedov@email.cz>
2020-05-10 12:21:29 +0200

    Request class

---
+++ src/main/java/me/sergeykuroedov/utils/Request.java
@@ -0,0 +1,43 @@
+package me.sergeykuroedov.utils;
+
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Response;
+
+import java.io.IOException;
+import java.util.Map;
+
+public class Request {
+    static OkHttpClient httpClient = new OkHttpClient();
+
+    public static String post(String url, Map<String, String> params) throws IOException {
+        FormBody.Builder builder = new FormBody.Builder();
+        params.forEach(builder::add);
+        FormBody formBody = builder.build();
+
+        okhttp3.Request request = new okhttp3.Request.Builder()
+                .url(url)
+                .post(formBody)
+                .build();
+        try (Response response = httpClient.newCall(request).execute()) {
+            if (!response.isSuccessful())
+                throw new IOException("Unexpected code " + response);
+            return response.body().string();
+        } catch (IOException e) {
+            throw e;
+        }
+    }
+
+    public static String get(String url) throws IOException {
+        okhttp3.Request request = new okhttp3.Request.Builder()
+                .url(url)
+                .get()
+                .build();
+        try (Response response = httpClient.newCall(request).execute()) {
+            return response.body().string();
+        } catch (IOException e) {
+            throw e;
+        }
+    }
+}
